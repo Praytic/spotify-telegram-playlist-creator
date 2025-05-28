af4cbf022c1c5492c9adec6e4dc348dab7c9590b
Praytic <chernogorovvm@gmail.com>
2025-05-28 11:45:28 -0700

    Modify scripts to work only locally

--- spotify.py
+++ spotify.py
@@ -1,15 +1,15 @@
-import tele
+from urllib.parse import quote
+
 import spotipy
-import spotipy.util as util
-from spotipy.oauth2 import SpotifyOAuth
-from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
-import os
+from spotipy.oauth2 import SpotifyOAuth
+
+import tele
 
 load_dotenv()
 
 scope = 'playlist-modify-public'
-username = os.getenv('USERNAME')
+username = 'zxop4ta6nk1bm47vu5pewdk67'
 
 auth_manager=SpotifyOAuth(scope=scope)
 spotify = spotipy.Spotify(auth_manager=auth_manager)
@@ -28,12 +28,12 @@ def user_playlist_get_or_create(username, name):
 if auth_manager:
     playlist_id = user_playlist_get_or_create(username, tele.playlist_name)
     tracks = []
-    for song in tele.songs:
-        print("Searching for song [{0}].".format(song))
-        results = spotify.search(q=song, limit=3)['tracks']['items']
+    for artist, song in tele.songs:
+        print(f"Searching for song [{song}] from artist [{artist}].")
+        results = spotify.search(q=quote(f"track:{song} artist:{artist}"), type="track", limit=1)['tracks']['items']
         if len(results) > 0:
             first_result = results[0]
-            uri, artist, song_name = first_result['uri'], first_result['album']['artists'][0]['name'], first_result['name']
+            uri = first_result['uri']
             tracks.append(first_result['uri'])
             print("Successfully got song [{0}].".format(song))
         else:
@@ -41,6 +41,7 @@ if auth_manager:
             print("Failed to get song [{0}].".format(song))
             tele.print_red("Failed to get song %s from spotify " %song)
 
+    print(f"Finished search for songs from telegram export. Stats: found {len(tracks)} tracks, not found {len(unfound)} tracks. Creating Spotify playlist.")
     paginate = 0
     while paginate < len(tracks):
         next_page = paginate + 100
@@ -48,5 +49,3 @@ if auth_manager:
         paginate = next_page
         print("Add [{0}] songs to playlist [{1}].".format(len(paged_tracks), playlist_id))
         spotify.playlist_add_items(playlist_id, paged_tracks)
-
-tele.save_file(unfound, "failed_spotify")
--- tele.py
+++ tele.py
@@ -1,9 +1,9 @@
-from telethon.sync import TelegramClient
-from telethon.tl.types import MessageMediaPhoto
 import sys
-import json
+
 from colorama import init
-from termcolor import colored, cprint
+from telethon.sync import TelegramClient
+from telethon.tl.types import DocumentAttributeAudio
+from termcolor import cprint
 
 init()
 
@@ -12,45 +12,22 @@ api_id = args[0]
 api_hash = args[1]
 name = 'Praytic Telegram Client'
 chat = args[2]
-playlist_name = args[3]
 songs = []
 performer = 'artist'
 title = 'track'
 test = 0
 broken_songs = []
+playlist_name = 'Счастье'
 
-print_red_onwhite  = lambda x: cprint(x, 'red', 'on_white')
+print_red_onwhite = lambda x: cprint(x, 'red', 'on_white')
 print_green_onwhite = lambda x: cprint(x, 'green', 'on_white')
 print_red = lambda x: cprint(x, 'red')
 
-def format_for_spotify(query_str):
-    return query_str.replace("&", ",").replace("feat.", ",").replace("feat", ",").replace("...", "").replace("..", "").replace("•", "").replace("/","").replace(".", " .")
-
-def save_file(songs, filename):
-    print("Saving songs to file")
-    with open(filename + ".json", "w", encoding="utf-8") as file:
-        json.dump(songs, file, ensure_ascii=False, separators=(",\n", ": "))
-
 with TelegramClient(name, api_id, api_hash) as client:
-    for message in client.iter_messages(chat):
+    for message in client.iter_messages(int(chat)):
         media = message.media
         if media:
             if hasattr(media, 'document'):
                 attributes = media.document.attributes[0]
-                performers, title = "", ""
-                try:
-                    if hasattr(attributes, 'performer') and attributes.performer != "" and attributes.performer:
-                        if attributes.title:
-                            title = attributes.title
-                        search_str = title + ' ' + performers
-                        search_str = format_for_spotify(search_str)
-                        if search_str == " " or not search_str:
-                            search_str = message.message.split("\n")[-1]
-                        print("Appending search_str [{0}] to songs.".format(search_str))
-                        songs.append(search_str)
-                except Exception as ex:
-                    broken_str = performers + ' ' + title
-                    broken_songs.append(broken_str)
-                    print_red_onwhite("Failed to get song %s from telegram" %broken_str)
-
-save_file(broken_songs, "tele_fail")
+                if isinstance(attributes, DocumentAttributeAudio):
+                    songs.append((attributes.title, attributes.performer))
